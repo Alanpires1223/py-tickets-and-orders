bfa8eab08d402d72937ad76a41ce3eccf379f84e
Alan <alanpires1223@gmail.com>
2025-09-01 12:17:44 -0300

    feat: Adiciona modelos e relacionamentos que faltavam em db/models.py

--- db/models.py
+++ db/models.py
@@ -11,12 +11,47 @@ class User(AbstractUser):
 
 
 # ---------------------------
+# Genre
+# ---------------------------
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+# ---------------------------
+# Actor
+# ---------------------------
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+# ---------------------------
+# CinemaHall
+# ---------------------------
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+# ---------------------------
 # Movie
 # ---------------------------
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField(help_text="Duração em minutos")
+    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor)
 
     class Meta:
         indexes = [
@@ -25,6 +60,18 @@ class Movie(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+# ---------------------------
+# MovieSession
+# ---------------------------
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} - {self.show_time}"
 
 
 # ---------------------------
